e9bdf3eaabac591a1e3dda6151bfdc42ab779413
Angel Gonzalez <aegonzalezd@hotmail.com>
2025-07-29 01:12:29 -0600

    Loading empty project

---
+++ performance/src/main/java/com/nocturna/performance/NocturnaPerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.nocturna.performance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NocturnaPerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NocturnaPerformanceApplication.class, args);
+	}
+
+}
---
+++ performance/src/test/java/com/nocturna/performance/NocturnaPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocturna.performance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NocturnaPerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
